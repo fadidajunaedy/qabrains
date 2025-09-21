169417d4311d5347849c98f06c9ee6b6296fee3f
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-09-21 14:18:13 +0700

    ADDING LOG

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from utils.logger import get_logger
+logger = get_logger()
 
 class LoginPage:
   def __init__(self, driver):
@@ -8,11 +10,16 @@ class LoginPage:
     self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
 
   def enter_email(self, email):
+    logger.info("Input email")
+    logger.debug(f"Locator used: {self.email_input}")
     self.driver.find_element(*self.email_input).send_keys(email)
 
   def enter_password(self, password):
+    logger.info("Input password")
+    logger.debug(f"Locator used: {self.password_input}")
     self.driver.find_element(*self.password_input).send_keys(password)
 
   def click_login(self):
+    logger.info("Click login button")
+    logger.debug(f"Locator used: {self.login_button}")
     self.driver.find_element(*self.login_button).click()
-    print("terclick")
