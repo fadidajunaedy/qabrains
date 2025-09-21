ce80fd301581d96ec99806157a4127d438804f61
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-09-21 12:48:04 +0700

    ADDING CONFIGURATION TEST

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def browser():
+  # setup (before test)
+  service = Service(ChromeDriverManager().install())
+  driver = webdriver.Chrome(service=service)
+  driver.maximize_window()
+  driver.implicitly_wait(5)
+  yield driver
+
+  # teardown (after test)
+  driver.quit()
