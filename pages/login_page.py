970a85ede5f4346573062f0b5ebc72abb1979e45
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-09-22 11:11:37 +0700

    CHANGES HOW USING LOGGER, AND DELETE IMPORT

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,25 +1,24 @@
 from selenium.webdriver.common.by import By
-from utils.logger import get_logger
-logger = get_logger()
 
 class LoginPage:
-  def __init__(self, driver):
+  def __init__(self, driver, logger):
     self.driver = driver
+    self.logger = logger
     self.email_input = (By.ID, "email")
     self.password_input = (By.ID, "password")
     self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
 
   def enter_email(self, email):
-    logger.info("Input email")
-    logger.debug(f"Locator used: {self.email_input}")
+    self.logger.info("Input email")
+    self.logger.debug(f"Locator used: {self.email_input}")
     self.driver.find_element(*self.email_input).send_keys(email)
 
   def enter_password(self, password):
-    logger.info("Input password")
-    logger.debug(f"Locator used: {self.password_input}")
+    self.logger.info("Input password")
+    self.logger.debug(f"Locator used: {self.password_input}")
     self.driver.find_element(*self.password_input).send_keys(password)
 
   def click_login(self):
-    logger.info("Click login button")
-    logger.debug(f"Locator used: {self.login_button}")
+    self.logger.info("Click login button")
+    self.logger.debug(f"Locator used: {self.login_button}")
     self.driver.find_element(*self.login_button).click()
--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -2,14 +2,12 @@ from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from utils.logger import get_logger
-logger = get_logger()
 
-def test_login_valid(browser):
+def test_login_valid(browser, logger):
   logger.info("=== [START] TEST LOGIN VALID ===")
   try:
     browser.get("https://practice.qabrains.com")
-    page = LoginPage(browser)
+    page = LoginPage(browser, logger)
     page.enter_email("qa_testers@qabrains.com")
     page.enter_password("Password123")
     page.click_login()
@@ -26,11 +24,11 @@ def test_login_valid(browser):
   finally:
     logger.info("=== [END] TEST LOGIN VALID ===")
 
-def test_login_invalid(browser):
+def test_login_invalid(browser, logger):
   logger.info("=== [START] TEST LOGIN INVALID ===")
   try:
     browser.get("https://practice.qabrains.com")
-    page = LoginPage(browser)
+    page = LoginPage(browser, logger)
     page.enter_email("wrong_email@mail.com")
     page.enter_password("wrong_pass")
     page.click_login()
