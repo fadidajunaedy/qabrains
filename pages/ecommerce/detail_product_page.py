4ad1e96d801503c146041b5872d251f921f5f8be
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-09-27 21:48:11 +0700

    ADDING FUNCTION FOR INTERACTION

--- pages/ecommerce/detail_product_page.py
+++ pages/ecommerce/detail_product_page.py
@@ -8,3 +8,20 @@ class DetailProductPage:
     self.decrease_quantity_button = (By.XPATH, "//button[text()='−']")
     self.increase_quantity_button = (By.XPATH, "//button[text()='+']")
     self.add_to_cart_button = (By.XPATH, "//button[text()='Add to cart']")
+
+  def increase_quantity_product(self):
+    self.logger.info("Clicking increase quantity button")
+    self.logger.debug(f"Locator used: {self.increase_quantity_button}")
+    increase_button = self.driver.find_element(*self.increase_quantity_button)
+    increase_button.click()
+
+  def decrease_quantity_product(self):
+    self.logger.info("Clicking decrease quantity button")
+    self.logger.debug(f"Locator used: {self.decrease_quantity_button}")
+    decrease_button = self.driver.find_element(*self.decrease_quantity_button)
+    decrease_button.click()
+
+  def add_product_to_cart(self):
+    self.logger.info("Clicking add to cart button")
+    self.logger.debug(f"Locator used: {self.add_to_cart_button}")
+    self.driver.find_element(*self.add_to_cart_button).click()
