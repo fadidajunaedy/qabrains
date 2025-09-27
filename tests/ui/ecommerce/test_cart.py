dc83a27f289807629521d3840a0fdced8c514621
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-09-27 21:49:10 +0700

    UPDATE test_add_to_cart_from_detail_product_page FUNCTION

--- tests/ui/ecommerce/test_cart.py
+++ tests/ui/ecommerce/test_cart.py
@@ -1,5 +1,6 @@
 from utils.ecommerce.login import login
 from pages.ecommerce.home_page import HomePage
+from pages.ecommerce.detail_product_page import DetailProductPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -40,3 +41,13 @@ def test_add_to_cart_from_detail_product_page(browser, logger):
   assert detail_wrapper.find_element(By.XPATH, ".//h1").text == "Sample Shirt Name"
   assert "/product-details" in browser.current_url
 
+  detail_product_page = DetailProductPage(browser, logger)
+  detail_product_page.increase_quantity_product()
+  detail_product_page.decrease_quantity_product()
+  detail_product_page.add_product_to_cart()
+
+  notifications_section = WebDriverWait(browser, 2).until(
+    EC.presence_of_element_located((By.XPATH, "//section[contains(@aria-label, 'Notifications')]"))
+  )
+  notification_message = notifications_section.find_element(By.XPATH, ".//ol/li/div/div")
+  assert notification_message.text == "Added to cart"
