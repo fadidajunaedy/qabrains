329a03a8f1f8b43513cbee8252c6d1a06ed30851
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-10-01 15:26:08 +0700

    REFACTORING CODE FOR HANDLE GET NOTIFICATION

--- tests/ui/ecommerce/favorite/test_add_favorite.py
+++ tests/ui/ecommerce/favorite/test_add_favorite.py
@@ -1,19 +1,18 @@
-from utils.ecommerce.login import login
-from pages.ecommerce.home_page import HomePage
-from pages.ecommerce.detail_page import DetailPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from pages.ecommerce.home_page import HomePage
+from pages.ecommerce.detail_page import DetailPage
+
+from utils.ecommerce.login import login
+from utils.ecommerce.get_notification import get_notification
 
 def test_add_favorite_from_home_page(browser, logger):
   login(browser, logger)
   page = HomePage(browser, logger)
   page.toggling_favorite("Sample Shirt Name")
-  notifications_section = WebDriverWait(browser, 2).until(
-    EC.presence_of_element_located((By.XPATH, "//section[contains(@aria-label, 'Notifications')]"))
-  )
-  notification_message = notifications_section.find_element(By.XPATH, ".//ol/li/div/div")
-  assert notification_message.text == "Added to favorites"
+  assert get_notification(browser) == "Added to favorites"
 
 def test_add_favorite_from_detail_page(browser, logger):
   login(browser, logger)
@@ -28,9 +27,4 @@ def test_add_favorite_from_detail_page(browser, logger):
 
   detail_page = DetailPage(browser, logger)
   detail_page.toggling_favorite()
-
-  notifications_section = WebDriverWait(browser, 2).until(
-    EC.presence_of_element_located((By.XPATH, "//section[contains(@aria-label, 'Notifications')]"))
-  )
-  notification_message = notifications_section.find_element(By.XPATH, ".//ol/li/div/div")
-  assert notification_message.text == "Added to favorites"
+  assert get_notification(browser) == "Added to favorites"
