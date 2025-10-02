7521ba7798945440abd9d194fa9c07059955a5d1
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-10-02 13:36:25 +0700

    RENAMING FUNCTION

--- pages/ecommerce/detail_page.py
+++ pages/ecommerce/detail_page.py
@@ -4,15 +4,15 @@ class DetailPage:
   def __init__(self, driver, logger):
     self.driver = driver
     self.logger = logger
-    self.add_to_favorite_button = (By.XPATH, "//span[@role='button']/button")
+    self.favorite_toggle_button = (By.XPATH, "//span[@role='button']/button")
     self.decrease_quantity_button = (By.XPATH, "//button[text()='−']")
     self.increase_quantity_button = (By.XPATH, "//button[text()='+']")
     self.add_to_cart_button = (By.XPATH, "//button[text()='Add to cart']")
 
-  def add_to_favorite(self):
+  def toggling_favorite(self):
     self.logger.info("Clicking Add to favorite button")
-    self.logger.debug(f"Locator used: {self.add_to_favorite_button}")
-    self.driver.find_element(*self.add_to_favorite_button).click()
+    self.logger.debug(f"Locator used: {self.favorite_toggle_button}")
+    self.driver.find_element(*self.favorite_toggle_button).click()
 
   def increase_quantity(self):
     self.logger.info("Clicking increase quantity button")
