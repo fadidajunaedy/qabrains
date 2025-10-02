c715f7cfe4e5ddf79d6f28031010814affa3d3d1
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-10-02 13:51:36 +0700

    ADDING FUNCTION FOR GETTING QUANTITY VALUE

--- pages/ecommerce/detail_page.py
+++ pages/ecommerce/detail_page.py
@@ -5,6 +5,7 @@ class DetailPage:
     self.driver = driver
     self.logger = logger
     self.favorite_toggle_button = (By.XPATH, "//span[@role='button']/button")
+    self.quantity_input = (By.XPATH, "//input[@readonly]")
     self.decrease_quantity_button = (By.XPATH, "//button[text()='−']")
     self.increase_quantity_button = (By.XPATH, "//button[text()='+']")
     self.add_to_cart_button = (By.XPATH, "//button[text()='Add to cart']")
@@ -13,6 +14,11 @@ class DetailPage:
     self.logger.info("Clicking Add to favorite button")
     self.logger.debug(f"Locator used: {self.favorite_toggle_button}")
     self.driver.find_element(*self.favorite_toggle_button).click()
+
+  def get_quantity(self):
+    self.logger.info("Getting quantity")
+    self.logger.debug(f"Locator used: {self.quantity_input}")
+    return self.driver.find_element(*self.quantity_input).get_attribute("value")
 
   def increase_quantity(self):
     self.logger.info("Clicking increase quantity button")
