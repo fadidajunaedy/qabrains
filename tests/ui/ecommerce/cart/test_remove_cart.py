7b4438db42eec6f92a787afd09fe38c531505828
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-10-08 09:56:30 +0700

    REFACTORING CODE

--- tests/ui/ecommerce/cart/test_remove_cart.py
+++ tests/ui/ecommerce/cart/test_remove_cart.py
@@ -14,12 +14,14 @@ def test_remove_from_cart_from_home_page(browser, logger):
   home_page = HomePage(browser, logger)
   home_page.add_to_cart("Sample Shirt Name")
   browser.implicitly_wait(2)
+
   home_page.remove_from_cart("Sample Shirt Name")
   assert get_notification(browser) == "Removed from cart"
 
 def test_remove_from_cart_from_cart_page_via_remove_button(browser, logger):
   login(browser, logger)
   home_page = HomePage(browser, logger)
+
   home_page.add_to_cart("Sample Shirt Name")
   assert get_notification(browser) == "Added to cart"
 
@@ -33,6 +35,7 @@ def test_remove_from_cart_from_cart_page_via_remove_button(browser, logger):
   cart_page = CartPage(browser, logger)
   cart_page.click_remove("Sample Shirt Name")
   assert get_dialog_title(browser) == "Are you absolutely sure?"
+
   cart_page.click_confirmation_remove()
   assert browser.find_element(By.XPATH, "//h1").text == "Your cart is empty."
 
@@ -43,15 +46,16 @@ def test_remove_from_cart_from_cart_page_via_decrease_quantity_button(browser, l
   assert get_notification(browser) == "Added to cart"
 
   home_page.click_cart()
-  cart_wrapper = WebDriverWait(browser, 5).until(
+  WebDriverWait(browser, 5).until(
     EC.presence_of_element_located((By.XPATH, "//div[@id='cart']"))
   )
-  assert cart_wrapper.find_element(By.XPATH, ".//h3").text == "Your Cart"
   assert "/cart" in browser.current_url
 
   cart_page = CartPage(browser, logger)
   assert cart_page.get_quantity("Sample Shirt Name") == 1
+
   cart_page.click_decrease_quantity("Sample Shirt Name")
   assert get_dialog_title(browser) == "Are you absolutely sure?"
+
   cart_page.click_confirmation_remove()
   assert browser.find_element(By.XPATH, "//h1").text == "Your cart is empty."
