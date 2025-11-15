b2fc535c13bdb846ae8e6c4c34c8931fc8421847
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-11-15 14:21:48 +0700

    REFACTORING CODE BY SIMPLFIED THE LOCATOR

--- utils/ecommerce/get_notification.py
+++ utils/ecommerce/get_notification.py
@@ -2,9 +2,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def get_notification(browser, timeout=2):
-  notifications_section = WebDriverWait(browser, timeout).until(
-    EC.presence_of_element_located((By.XPATH, "//section[contains(@aria-label, 'Notifications')]"))
+def get_notification(browser, timeout=5):
+  toast = WebDriverWait(browser, timeout).until(
+    EC.visibility_of_element_located((By.XPATH, "//li[@data-sonner-toast and @data-visible='true']"))
   )
-  notification_message = notifications_section.find_element(By.XPATH, ".//ol/li/div/div")
-  return notification_message.text
+
+  toast_title = WebDriverWait(browser, timeout).until(
+    EC.visibility_of_element_located((By.XPATH, "//li[@data-sonner-toast and @data-visible='true']//div[@data-title]"))
+  )
+
+  notification_message = toast_title.text.strip()
+  return notification_message
