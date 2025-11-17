3df94ac67565977bcee316788aae8c3e6741f7ae
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-11-17 11:29:38 +0700

    ADDING FULLSCREEN WINDOW UTIL

--- utils/ecommerce/login.py
+++ utils/ecommerce/login.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def login(browser, logger):
   browser.get("https://practice.qabrains.com/ecommerce/login")
   browser.maximize_window()
+  browser.fullscreen_window()
   page = LoginPage(browser, logger)
   page.enter_email("test@qabrains.com")
   page.enter_password("Password123")
