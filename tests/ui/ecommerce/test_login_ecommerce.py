d7b69d2e5a5435fdc86cbf1c127219738a020eae
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-11-17 10:51:39 +0700

    RENAMING TEST LOGIN ECOMMERCE FILE

--- tests/ui/ecommerce/test_login_ecommerce.py
+++ tests/ui/ecommerce/test_login_ecommerce.py
@@ -1,10 +1,11 @@
-from pages.ecommerce.login_page import LoginPage
-from pages.ecommerce.home_page import HomePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_login_valid(browser, logger):
+from pages.ecommerce.home_page import HomePage
+from pages.ecommerce.login_page import LoginPage
+
+def test_login_ecommerce_valid(browser, logger):
   logger.info("=== [START] TEST LOGIN VALID ===")
   try:
     browser.get("https://practice.qabrains.com/ecommerce/login")
@@ -25,7 +26,7 @@ def test_login_valid(browser, logger):
   finally:
     logger.info("=== [END] TEST LOGIN VALID ===")
 
-def test_login_email_invalid(browser, logger):
+def test_login_ecommerce_email_invalid(browser, logger):
   logger.info("=== [START] TEST LOGIN EMAIL INVALID ===")
   try:
     browser.get("https://practice.qabrains.com/ecommerce/login")
@@ -44,7 +45,7 @@ def test_login_email_invalid(browser, logger):
   finally:
     logger.info("=== [END] TEST LOGIN EMAIL INVALID ===")
 
-def test_login_password_invalid(browser, logger):
+def test_login_ecommerce_password_invalid(browser, logger):
   logger.info("=== [START] TEST LOGIN PASSWORD INVALID ===")
   try:
     browser.get("https://practice.qabrains.com/ecommerce/login")
@@ -63,7 +64,7 @@ def test_login_password_invalid(browser, logger):
   finally:
     logger.info("=== [END] TEST LOGIN PASSWORD INVALID ===")
 
-def test_login_invalid(browser, logger):
+def test_login_ecommerce_invalid(browser, logger):
   logger.info("=== [START] TEST LOGIN INVALID ===")
   try:
     browser.get("https://practice.qabrains.com/ecommerce/login")
@@ -82,7 +83,7 @@ def test_login_invalid(browser, logger):
   finally:
     logger.info("=== [END] TEST LOGIN INVALID ===")
 
-def test_login_valid_then_logout(browser, logger):
+def test_login_ecommerce_valid_then_logout(browser, logger):
   logger.info("=== [START] TEST LOGIN VALID THEN LOGOUT ===")
   try:
     browser.get("https://practice.qabrains.com/ecommerce/login")
