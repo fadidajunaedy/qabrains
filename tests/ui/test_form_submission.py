9ce6082c6dd4b5b83ec2c2ccd10851c482af3ee1
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-11-17 11:28:57 +0700

    ADDING OS ABSOLUTE PATH FOR FILE INPUT AND ADDING BROWSER MAXIMIZE AND FULLSCREEN WINDOW

--- tests/ui/test_form_submission.py
+++ tests/ui/test_form_submission.py
@@ -1,18 +1,23 @@
-from pages.form_submission_page import FormSubmissionPage
+import os
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from pages.form_submission_page import FormSubmissionPage
 
 def test_form_submission_valid(browser, logger):
   logger.info("=== [START] TEST FORM SUBMISSION VALID ===")
   try:
     browser.get("https://practice.qabrains.com/form-submission")
+    browser.maximize_window()
+    browser.fullscreen_window()
     page = FormSubmissionPage(browser, logger)
     page.enter_name("Fadida Zanetti Junaedy")
     page.enter_email("fadidajunaedy@gmail.com")
     page.enter_contact("089501437800")
     page.enter_date("2025-09-16")
-    page.enter_file("C:/Users/Fadida Junaedy/OneDrive/Documents/code.png")
+    page.enter_file(os.path.abspath("assets/dummy.png"))
     page.enter_color("Red")
     page.enter_food("Pasta")
     page.enter_country("Indonesia")
