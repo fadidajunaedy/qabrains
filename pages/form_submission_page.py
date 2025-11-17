e7a4f2e32844e61ae459c551c764aadeb78a21d5
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-11-17 11:26:49 +0700

    ADDING JAVASCRIPT SCROLL TO HANDLING CLICK INTERCEPTED

--- pages/form_submission_page.py
+++ pages/form_submission_page.py
@@ -17,48 +17,62 @@ class FormSubmissionPage:
   def enter_name(self, name):
     self.logger.info("Input name")
     self.logger.debug(f"Locator used: {self.name_input}")
-    self.driver.find_element(*self.name_input).send_keys(name)
+    name_input = self.driver.find_element(*self.name_input)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", name_input)
+    name_input.send_keys(name)
 
   def enter_email(self, email):
     self.logger.info("Input email")
     self.logger.debug(f"Locator used: {self.email_input}")
-    self.driver.find_element(*self.email_input).send_keys(email)
+    email_input = self.driver.find_element(*self.email_input)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", email_input)
+    email_input.send_keys(email)
 
   def enter_contact(self, contact):
     self.logger.info("Input contact")
     self.logger.debug(f"Locator used: {self.contact_input}")
-    self.driver.find_element(*self.contact_input).send_keys(contact)
+    contact_input = self.driver.find_element(*self.contact_input)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", contact_input)
+    contact_input.send_keys(contact)
 
   def enter_date(self, date):
     self.logger.info("Input date")
     self.logger.debug(f"Locator used: {self.date_input}")
-    self.driver.find_element(*self.date_input).send_keys(date)
+    date_input = self.driver.find_element(*self.date_input)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", date_input)
+    date_input.send_keys(date)
 
   def enter_file(self, file):
     self.logger.info("Input file")
     self.logger.debug(f"Locator used: {self.file_input}")
-    self.driver.find_element(*self.file_input).send_keys(file)
+    file_input = self.driver.find_element(*self.file_input)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", file_input)
+    file_input.send_keys(file)
 
   def enter_color(self, color):
     self.logger.info("Input color")
     self.logger.debug(f"Locator used: {self.color_select}")
     group_color = self.driver.find_element(*self.color_select)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", group_color)
     group_color.find_element(By.XPATH, f"//label[@for='{color}']").click()
 
   def enter_food(self, food):
     self.logger.info("Input food")
     self.logger.debug(f"Locator used: {self.food_select}")
     group_food = self.driver.find_element(*self.food_select)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", group_food)
     group_food.find_element(By.XPATH, f"//label[@for='{food}']").click()
 
   def enter_country(self, country):
     self.logger.info("Select country")
     self.logger.debug(f"Locator used: {self.country_select}")
-
     select = self.driver.find_element(*self.country_select)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", select)
     select.find_element(By.XPATH, f"//option[@value='{country}']").click()
 
   def click_submit(self):
     self.logger.info("Click submit button")
     self.logger.debug(f"Locator used: {self.submit_button}")
-    self.driver.find_element(*self.submit_button).click()
+    submit_button = self.driver.find_element(*self.submit_button)
+    self.driver.execute_script("arguments[0].scrollIntoView('alignToTop');", submit_button)
+    submit_button.click()
